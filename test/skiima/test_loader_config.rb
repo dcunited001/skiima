bf63bf35bd4c044ea691a88cd0ed101aceda8e50
David Conner <dconner.pro@gmail.com>
2011-11-13 23:52:30 -0500

    changed format of config

--- test/skiima/test_loader_config.rb
+++ test/skiima/test_loader_config.rb
@@ -2,7 +2,10 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class TestLoaderConfig < Skiima::Test
   def setup
-    @loader_config = Skiima::LoaderConfig.new
+    @loader_config = Skiima::LoaderConfig.new(
+      :locale => Skiima.locale_file,
+      :config => Skiima.config_file,
+      :depends => Skiima.depends_file)
   end
 
   def test_locale_is_set
