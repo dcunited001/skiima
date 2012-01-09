ebe0eb8e6ecc4362334cf55d183f2f0cdb25cd75
David Conner <dconner.pro@gmail.com>
2012-01-09 01:48:51 -0500

    removed MiniTest::Unit

--- test/skiima/sql_object/test_base.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../../../test_helper', __FILE__)
-
-module Skiima
-  module SqlObject
-    class TestBase < Skiima::Test
-
-    end
-  end
-end
--- test/skiima/test_base.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../../test_helper', __FILE__)
-
-class TestBase < Skiima::Test
-  def setup
-
-  end
-
-
-end
