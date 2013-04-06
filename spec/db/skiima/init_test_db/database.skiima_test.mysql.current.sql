8cb4093dc4622127fc933191244862b0ed8f51e3
David Conner <dconner.pro@gmail.com>
2013-04-06 17:18:06 -0600

    updated sql scripts create & drop the test database/user

--- spec/db/skiima/init_test_db/database.skiima_test.mysql.current.sql
+++ spec/db/skiima/init_test_db/database.skiima_test.mysql.current.sql
@@ -1,2 +1,7 @@
 DROP DATABASE IF EXISTS &database;
+--================
+CREATE USER '&testuser'@'localhost' IDENTIFIED BY '&testpass';
+--================
 CREATE DATABASE &database;
+--================
+GRANT ALL PRIVILEGES ON &database.* TO '&testuser'@'localhost';
--- spec/db/skiima/init_test_db/database.skiima_test.postgresql.current.sql
+++ spec/db/skiima/init_test_db/database.skiima_test.postgresql.current.sql
@@ -1,2 +1,7 @@
 DROP DATABASE IF EXISTS &database;
+--================
+CREATE USER &testuser WITH PASSWORD '&testpass';
+--================
 CREATE DATABASE &database;
+--================
+GRANT ALL PRIVILEGES ON DATABASE &database to &testuser;
