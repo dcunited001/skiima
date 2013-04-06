e3a5e8d22e1574f2701dd239f80b3928260f2b35
David Conner <dconner.pro@gmail.com>
2013-04-06 16:50:37 -0600

    Skiima::Config module: split skiima config methods into a module which is loaded onto Skiima & Skiima::loader

---
+++ spec/skiima/config_spec.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Skiima::Config do
+  # #config, #config=,
+  # #full_scripts_path, #full_database_path, #full_depends_path
+  # #read_sql_file, #read_db_yml, #read_depends_yaml
+  # #read_yml_or_throw, #symbolize_keys, #interpolate_sql
+
+  # create test class -or- describe implementing classes?
+  #describe Skiima do
+  #
+  #end
+  #
+  #describe Skiima::Loader do
+  #
+  #end
+end
