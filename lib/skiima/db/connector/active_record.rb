73bea67c637e32cb4a45823235d2706bdfe0a7f1
David Conner <dconner.pro@gmail.com>
2013-04-07 06:21:57 -0600

    remove gem include form Skiima::Db::Connector::ActiveRecord

--- lib/skiima/db/connector/active_record.rb
+++ lib/skiima/db/connector/active_record.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 
-gem :active_record, '>= 3.2, < 4.1'
-
 module Skiima
-  module DbConnector
-    module ActiveRecord
+  module Db
+    module Connector
+      module ActiveRecord
 
+      end
     end
   end
 end
