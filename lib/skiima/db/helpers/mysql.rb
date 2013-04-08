95a28bfc46d9edd130e84fafe501de4412c231b6
David Conner <dconner.pro@gmail.com>
2013-04-08 00:23:52 -0600

    removed DbAdapter classes

--- lib/skiima/db/helpers/mysql.rb
+++ lib/skiima/db/helpers/mysql.rb
@@ -207,3 +207,25 @@ module Skiima
     end
   end
 end
+
+## encoding: utf-8
+#module Skiima
+#  module DbAdapters
+#    class BaseMysqlAdapter < Base
+#      attr_accessor :version
+#
+#      LOST_CONNECTION_ERROR_MESSAGES = [
+#          "Server shutdown in progress",
+#          "Broken pipe",
+#          "Lost connection to MySQL server during query",
+#          "MySQL server has gone away" ]
+#
+#      # FIXME: Make the first parameter more similar for the two adapters
+#      def initialize(connection, logger, connection_options, config)
+#        super(connection, logger)
+#        @connection_options, @config = connection_options, config
+#        @quoted_column_names, @quoted_table_names = {}, {}
+#      end
+#    end
+#  end
+#end
